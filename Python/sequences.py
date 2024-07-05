0f218f3aa6f4dbe71babdd2eedf0f96e1ffd7c84
sumeetbidhan <sumeetbidhanwork@gmail.com>
2024-07-06 00:19:59 +0530

    new

---
+++ Python/sequences.py
@@ -0,0 +1,12 @@
+name = "Harry"
+
+print(name[0])
+
+coordinate = (10.0, 20.0)
+
+print(coordinate)
+
+# list - sequence of mutable values
+# tuple - sequence of immutable values
+# set - collection of unique values
+# dict - collection of key-value pairs
