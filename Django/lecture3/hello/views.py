fda9eb2554e5a1f33e0bba71c2eaaa8b509dc21b
sumeetbidhan <sumeetbidhanwork@gmail.com>
2024-07-08 01:11:15 +0530

    new

--- Django/lecture3/hello/views.py
+++ Django/lecture3/hello/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+  return HttpResponse("Hello, world!")
+
+
+def brian(request):
+  return HttpResponse("Hello, Brain!")
+
+def david(request):
+  return HttpResponse("Hello, David!")
+
+def greet(request, name):
+  return HttpResponse(f"Hello, {name}!")
