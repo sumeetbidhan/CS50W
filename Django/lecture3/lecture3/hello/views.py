f31f522ab92cb164daf6dc7d0167838148c6893a
sumeetbidhan <sumeetbidhanwork@gmail.com>
2024-07-08 17:02:44 +0530

    new

--- Django/lecture3/lecture3/hello/views.py
+++ Django/lecture3/lecture3/hello/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-  return HttpResponse("Hello, world!")
+  return render(request, "hello/index.html")
 
 
 def brian(request):
@@ -13,5 +13,10 @@ def brian(request):
 def david(request):
   return HttpResponse("Hello, David!")
 
+'''def greet(request, name):
+  return HttpResponse(f"Hello, {name.capitalize()}!")'''
+
 def greet(request, name):
-  return HttpResponse(f"Hello, {name.capitalize()}!")
+  return render(request, "hello/greet.html"),{
+    "name": name.capitalize()
+  }
--- Django/newyear/newyear/settings.py
+++ Django/newyear/newyear/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'newyear1',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
